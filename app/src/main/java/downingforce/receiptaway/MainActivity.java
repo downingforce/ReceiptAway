a274b7d26b3a238bcd5191f1d436771ba3ddc2f2
Jason Downing <downingforce@gmail.com>
2016-12-22 10:08:03 -0800

    add copyFiles to MainActivity for accessing AssetManager and read/write byte streams

--- app/src/main/java/downingforce/receiptaway/MainActivity.java
+++ app/src/main/java/downingforce/receiptaway/MainActivity.java
@@ -1,11 +1,18 @@
 package downingforce.receiptaway;
 
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +32,30 @@ public class MainActivity extends AppCompatActivity {
         String lang = "eng";
         mTess = new TessBaseAPI();
         mTess.init(datapath, lang);
+    }
+    private void copyFiles() {
+        try {
+            String filepath = datapath + "/tessdata/eng.traineddata";
+
+            AssetManager assetManager = getAssets();
+
+            InputStream instream = assetManager.open("tessdata/eng.traineddata");
+            OutputStream outstream = new FileOutputStream(filepath);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = instream.read(buffer)) != -1) {
+                outstream.write(buffer, 0, read);
+            }
+            outstream.flush();
+            outstream.close();
+            instream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
